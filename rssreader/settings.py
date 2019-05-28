b78b65dcf6b67e808c1cf0903b30cd0e5c80bc4d
None None <omctee@gmail.com>
2019-05-27 21:03:09 -0700

    update

--- rssreader/settings.py
+++ rssreader/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -134,3 +135,5 @@ STATICFILES_DIRS = (
 python manage.py collectstatic
 
 """
+# Activate Django-Heroku.
+django_heroku.settings(locals())
